ceddad10792b657609392fb972ac3cbd3f0cbfcd
MacMillan <lanny.macmillan@thryv.com>
2023-04-13 21:45:23 -0400

    started tetris clone game

---
+++ tetris_clone/src/components/Game.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Game = ({ rows, columns }) => {
+  return (
+    <div>
+      <p>
+        Rows {rows} Columns {columns}
+      </p>
+    </div>
+  );
+};
+
+export default Game;
