cfb5c826b1b460c9b8840335151f652b1703bbb2
MacMillan <lanny.macmillan@thryv.com>
2023-04-14 09:54:24 -0400

    board, tetrominos, preview and player piece done and working

--- tetris_clone/src/hooks/useBoard.js
+++ tetris_clone/src/hooks/useBoard.js
@@ -1,7 +1,25 @@
-import { useState } from "react";
-import { buildBoard } from "../util/Board";
+import { useEffect, useState } from "react";
+import { buildBoard, nextBoard } from "../util/Board";
 
-export const useBoard = ({ rows, columns }) => {
+export const useBoard = ({
+  rows,
+  columns,
+  player,
+  resetPlayer,
+  addLinesCleared,
+}) => {
   const [board, setBoard] = useState(buildBoard({ rows, columns }));
+
+  useEffect(() => {
+    setBoard((previousBoard) =>
+      nextBoard({
+        board: previousBoard,
+        player,
+        resetPlayer,
+        addLinesCleared,
+      })
+    );
+  }, [player, resetPlayer, addLinesCleared]);
+
   return [board];
 };
--- tetris_clone/src/util/Board.js
+++ tetris_clone/src/util/Board.js
@@ -1,4 +1,5 @@
 import { defaultCell } from "./Cell";
+import { transferToBoard } from "./Tetrominoes";
 
 export const buildBoard = ({ rows, columns }) => {
   // Take the rows and columns, make an array for the rows, for each row we make an array for the columns and than in each column we set up a default cell eventually return a board with rows and size with rows and columns
@@ -9,5 +10,29 @@ export const buildBoard = ({ rows, columns }) => {
   return {
     rows: builtRows,
     size: { rows, columns },
+  };
+};
+
+export const nextBoard = ({ board, player, resetPlayer, addLinesCleared }) => {
+  const { tetromino, position } = player;
+
+  // Copy and clear spaces used by pieces that
+  // hadn't collided and occupied spaces permanently
+  let rows = board.rows.map((row) =>
+    row.map((cell) => (cell.occupied ? cell : { ...defaultCell }))
+  );
+
+  rows = transferToBoard({
+    className: tetromino.className,
+    isOccupied: player.collided,
+    position,
+    rows,
+    shape: tetromino.shape,
+  });
+
+  // Return the next board
+  return {
+    rows,
+    size: { ...board.size },
   };
 };
